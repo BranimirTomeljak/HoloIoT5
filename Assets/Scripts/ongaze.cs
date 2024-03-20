79f9792ad2f9069f716e776fead91cde07c2f147
BranimirTomeljak <mtomeljak@gmail.com>
2024-03-20 16:33:36 +0100

    testing eye tracking

---
+++ Assets/Scripts/ongaze.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ongaze : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        printaj();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public async void printaj()
+    {
+        Debug.Log("AAAAAA");
+    }
+}
